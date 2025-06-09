a0b33f54de1c7e8ea66a2713fe8c7a6a9105f675
quangvuong97 <vuongvuvo97@gmail.com>
2025-06-10 02:54:12 +0700

    update color

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -79,10 +79,10 @@ const Gallery: React.FC = () => {
             </h2>
             <div
               className="max-w-[200px] mx-auto relative
-            [&::before]:absolute [&::before]:left-[-70px] [&::before]:top-1/2 [&::before]:-translate-y-1/2 [&::before]:content-[''] [&::before]:w-[144px] [&::before]:h-[1px] [&::before]:bg-[#86a0b6]
-            [&::after]:absolute [&::after]:right-[-70px] [&::after]:top-1/2 [&::after]:-translate-y-1/2 [&::after]:content-[''] [&::after]:w-[144px] [&::after]:h-[1px] [&::after]:bg-[#86a0b6]"
+            [&::before]:absolute [&::before]:left-[-70px] [&::before]:top-1/2 [&::before]:-translate-y-1/2 [&::before]:content-[''] [&::before]:w-[144px] [&::before]:h-[1px] [&::before]:bg-[#1e8267]
+            [&::after]:absolute [&::after]:right-[-70px] [&::after]:top-1/2 [&::after]:-translate-y-1/2 [&::after]:content-[''] [&::after]:w-[144px] [&::after]:h-[1px] [&::after]:bg-[#1e8267]"
             >
-              <div className="absolute left-1/2 w-[15px] h-[15px] border border-[#86a0b6] rounded-full -translate-x-1/2 -top-[5px]"></div>
+              <div className="absolute left-1/2 w-[15px] h-[15px] border border-[#1e8267] rounded-full -translate-x-1/2 -top-[5px]"></div>
             </div>
           </div>
         </div>
--- src/components/Story.tsx
+++ src/components/Story.tsx
@@ -35,29 +35,29 @@ const Story: React.FC = () => {
             </h2>
             <div
               className="max-w-[200px] mx-auto relative
-            [&::before]:absolute [&::before]:left-[-70px] [&::before]:top-1/2 [&::before]:-translate-y-1/2 [&::before]:content-[''] [&::before]:w-[144px] [&::before]:h-[1px] [&::before]:bg-[#86a0b6]
-            [&::after]:absolute [&::after]:right-[-70px] [&::after]:top-1/2 [&::after]:-translate-y-1/2 [&::after]:content-[''] [&::after]:w-[144px] [&::after]:h-[1px] [&::after]:bg-[#86a0b6]"
+            [&::before]:absolute [&::before]:left-[-70px] [&::before]:top-1/2 [&::before]:-translate-y-1/2 [&::before]:content-[''] [&::before]:w-[144px] [&::before]:h-[1px] [&::before]:bg-[#1e8267]
+            [&::after]:absolute [&::after]:right-[-70px] [&::after]:top-1/2 [&::after]:-translate-y-1/2 [&::after]:content-[''] [&::after]:w-[144px] [&::after]:h-[1px] [&::after]:bg-[#1e8267]"
             >
-              <div className="absolute left-1/2 w-[15px] h-[15px] border border-[#86a0b6] rounded-full -translate-x-1/2 -top-[5px]"></div>
+              <div className="absolute left-1/2 w-[15px] h-[15px] border border-[#1e8267] rounded-full -translate-x-1/2 -top-[5px]"></div>
             </div>
           </div>
         </div>
         <div className="flex flex-wrap -mx-3 -mt-0">
           <div className="flex-1 flex-shrink-0 w-full max-w-full px-3 mt-0">
-            <div className="relative [&::after]:content-[''] [&::after]:bg-[#cee0ef] [&::after]:w-[2px] [&::after]:h-full [&::after]:absolute [&::after]:left-1/2 [&::after]:top-0 [&::after]:-translate-x-1/2">
+            <div className="relative [&::after]:content-[''] [&::after]:bg-[#1e8267] [&::after]:w-[2px] [&::after]:h-full [&::after]:absolute [&::after]:left-1/2 [&::after]:top-0 [&::after]:-translate-x-1/2">
               <div
                 className="bg-white w-[20px] h-[20px] absolute left-1/2 top-0 border border-[#738ea5] -translate-x-1/2 rounded-full z-[1]
-                [&::before]:absolute [&::before]:left-[2px] [&::before]:top-[2px] [&::before]:w-[14px] [&::before]:h-[14px] [&::before]:content-[''] [&::before]:bg-[#738ea5] [&::before]:rounded-full"
+                [&::before]:absolute [&::before]:left-[2px] [&::before]:top-[2px] [&::before]:w-[14px] [&::before]:h-[14px] [&::before]:content-[''] [&::before]:bg-[#1e8267] [&::before]:rounded-full"
               ></div>
               {story.map((e, index) => {
                 const check = index % 2;
                 const image = (
                   <div
                     className={
-                      `max-w-[330px] p-[10px] border border-dashed border-[#86a0b6] rounded-full relative z-[1] bg-white mx-auto` +
+                      `max-w-[330px] p-[10px] border border-dashed border-[#1e8267] rounded-full relative z-[1] bg-white mx-auto` +
                       (check
-                        ? " [&::before]:absolute [&::before]:left-auto [&::before]:-right-[102px] [&::before]:rotate-[102deg] [&::before]:top-[195px] [&::before]:w-[90%] [&::before]:h-[90%] [&::before]:border [&::before]:border-dashed [&::before]:border-[#738ea5] [&::before]:content-[''] [&::before]:rounded-full [&::before]:-z-[1] [&::before]:border-b-0 [&::before]:border-r-0 [&::before]:border-t-0 maxSsm:[&::before]:-right-[140px] maxXs:[&::before]:-right-[96px] maxSm:[&::before]:-right-[65px] left-auto [&::before]:-right-[102px] [&::before]:rotate-[102deg]"
-                        : " [&::before]:absolute [&::before]:top-[195px] [&::before]:w-[90%] [&::before]:h-[90%] [&::before]:border [&::before]:border-dashed [&::before]:border-[#738ea5] [&::before]:content-[''] [&::before]:rounded-full [&::before]:-z-[1] [&::before]:border-b-0 [&::before]:border-r-0 [&::before]:border-t-0 maxSsm:[&::before]:left-[-140px] maxXs:[&::before]:left-[-96px] [&::before]:left-[-103px] [&::before]:rotate-[66deg] maxSm:[&::before]:left-[-65px]")
+                        ? " [&::before]:absolute [&::before]:left-auto [&::before]:-right-[102px] [&::before]:rotate-[102deg] [&::before]:top-[195px] [&::before]:w-[90%] [&::before]:h-[90%] [&::before]:border [&::before]:border-dashed [&::before]:border-[#1e8267] [&::before]:content-[''] [&::before]:rounded-full [&::before]:-z-[1] [&::before]:border-b-0 [&::before]:border-r-0 [&::before]:border-t-0 maxSsm:[&::before]:-right-[140px] maxXs:[&::before]:-right-[96px] maxSm:[&::before]:-right-[65px] left-auto [&::before]:-right-[102px] [&::before]:rotate-[102deg]"
+                        : " [&::before]:absolute [&::before]:top-[195px] [&::before]:w-[90%] [&::before]:h-[90%] [&::before]:border [&::before]:border-dashed [&::before]:border-[#1e8267] [&::before]:content-[''] [&::before]:rounded-full [&::before]:-z-[1] [&::before]:border-b-0 [&::before]:border-r-0 [&::before]:border-t-0 maxSsm:[&::before]:left-[-140px] maxXs:[&::before]:left-[-96px] [&::before]:left-[-103px] [&::before]:rotate-[66deg] maxSm:[&::before]:left-[-65px]")
                     }
                   >
                     <img
@@ -86,7 +86,7 @@ const Story: React.FC = () => {
                     <h3 className="text-[22px] font-medium text-[#002642] mb-[0.5em] uppercase font-[Futura_PT] mt-0 leading-[1.2] maxSsm:text-[18px] maxLg:text-[20px]">
                       {e.title}
                     </h3>
-                    <span className="text-[14px] font-medium block mb-[15px] text-[#8ab1d2]">
+                    <span className="text-[14px] font-medium block mb-[15px] text-[#1e8267]">
                       Nov 12,2021
                     </span>
                     <p className="text-[16px] text-[#848892] leading-[1.8em] mt-0 mb-0">
@@ -110,9 +110,9 @@ const Story: React.FC = () => {
                       }
                     >
                       {!check && index > 0 ? (
-                        <span className="bg-[#e7eff6] w-[43px] h-[43px] leading-[41px] text-center absolute -right-[22px] -top-[22px] rounded-full z-10">
+                        <span className="bg-[#1e8267] w-[43px] h-[43px] leading-[41px] text-center absolute -right-[22px] -top-[22px] rounded-full z-10">
                           <i
-                            className={`text-[#738ea5] fi ${
+                            className={`text-[#fff] fi ${
                               storyIcons[index - 1]
                             }`}
                           ></i>
@@ -130,9 +130,9 @@ const Story: React.FC = () => {
                       }
                     >
                       {check && index > 0 ? (
-                        <span className="bg-[#e7eff6] w-[43px] h-[43px] leading-[41px] text-center absolute -top-[22px] rounded-full z-10 -left-[22px]">
+                        <span className="bg-[#1e8267] w-[43px] h-[43px] leading-[41px] text-center absolute -top-[22px] rounded-full z-10 -left-[22px]">
                           <i
-                            className={`text-[#738ea5] fi ${
+                            className={`text-[#fff] fi ${
                               storyIcons[index - 1]
                             }`}
                           ></i>
@@ -147,8 +147,8 @@ const Story: React.FC = () => {
               })}
               <div className="relative flex flex-wrap -mx-3 -mt-0 mt-[50px] pt-[15px] last:maxSsm:p-0 last:maxSsm:m-0">
                 <div className="flex-1 flex-shrink-0 w-full max-w-full px-3 mt-0 last:pl-[45px] last:pr-[100px] maxSm:last:pl-[45px] maxSm:last:pr-[70px] maxSsm:last:px-[15px] pt-0 relative maxSsm:border-0 maxSsm:pt-0 md:ml-[50%] md:flex-[0_0_auto] md:w-1/2">
-                  <span className="bg-[#e7eff6] w-[43px] h-[43px] leading-[41px] text-center absolute -left-[22px] -top-[22px] rounded-full z-10 maxSsm:hidden">
-                    <i className="text-[#738ea5] fi flaticon-wedding-rings"></i>
+                  <span className="bg-[#1e8267] w-[43px] h-[43px] leading-[41px] text-center absolute -left-[22px] -top-[22px] rounded-full z-10 maxSsm:hidden">
+                    <i className="text-[#fff] fi flaticon-wedding-rings"></i>
                   </span>
                 </div>
               </div>
